00f32ef41089017943ebab63f3b1eeafa1e81e09
David Grs <david.a.grs@gmail.com>
2019-01-30 14:00:37 +0100

    cleanup clobbers

--- tsc_clock.h
+++ tsc_clock.h
@@ -36,29 +36,27 @@ namespace detail
 
 inline void cpuid()
 {
-	uint64_t rax, rbx, rcx, rdx;
-	__asm__ __volatile__("cpuid" : "=a"(rax), "=b"(rbx), "=d"(rdx), "=c"(rcx));
-	asm volatile("" : : "r,m"(rax) : "memory");
+	asm volatile("cpuid" ::: "%rax", "%rbx", "%rcx", "%rdx");
 }
 
 inline uint64_t rdtsc()
 {
 	uint64_t rax, rdx;
-	__asm__ __volatile__ ("rdtsc" : "=a"(rax), "=d"(rdx));
+	asm volatile("rdtsc" : "=a"(rax), "=d"(rdx));
 	return (rdx << 32) + rax;
 }
 
 inline uint64_t rdtscp()
 {
 	uint64_t rax, rdx;
-	__asm__ __volatile__("rdtscp" : "=a"(rax), "=d"(rdx));
+	asm volatile("rdtscp" : "=a"(rax), "=d"(rdx) :: "%rcx");
 	return (rdx << 32) + rax;
 }
 
 inline uint64_t rdtscp(int& chip, int& core)
 {
 	uint64_t rax, rcx, rdx;
-	__asm__ __volatile__("rdtscp" : "=a"(rax), "=d"(rdx), "=c"(rcx));
+	asm volatile("rdtscp" : "=a"(rax), "=d"(rdx), "=c"(rcx));
 	chip = (rcx & 0xFFF000) >> 12;
 	core = rcx & 0xFFF;
 	return (rdx << 32) + rax;
